6467b57d87c691c90c9949f55872dc05e3539447
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-17 07:13:41 -0800

    Quiz

---
+++ Quiz-2-1.py
@@ -0,0 +1,17 @@
+#Question 2
+
+PosStatement=("Happy")
+
+Negstatment= ("You're not happy. Now SCRAM!")
+
+Not=("Unhappy")
+
+Happy=("Not")
+
+Angry=("You're not happy. Now SCRAM!")
+
+
+if PosStatement :print(Negstatment)
+
+if Not    :print(PosStatement)
+
---
+++ quiz-2-2.py
@@ -0,0 +1,13 @@
+#question 3
+
+
+
+A=("yOu have class")
+C=A
+E=A
+D=("YOudont have Class")
+F=("YouDont have Class")
+G=("You Dont have class")
+B=("YOu Dont have Class")
+X1=()   #Input wat day
+print(X1)
